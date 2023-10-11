dc22b3b8b4e5e3f50d1ffbe13be04a3e2a4bde86
franklin-salas <framklin00186@gmail.com>
2023-10-10 21:46:55 -0400

    add reducer

--- useTodos.js
+++ useTodos.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../components/todoReducer';
+import { todoReducer } from './todoReducer';
 // import { todoReducer } from '../08-useReducer/todoReducer';
 
 const init = () => {
